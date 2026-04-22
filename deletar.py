b7a5ff70cb38df05dacaefd20f36919ee5918a26
kauan <kauanmartinsesilva@gmail.com>
2026-04-21 21:27:23 -0300

    Cadastro de usuários implemetado

--- deletar.py
+++ deletar.py
@@ -1,3 +1,7 @@
 import sqlite3
 conn = sqlite3.connect('banco.db')
 cursor = conn.cursor()
+
+cursor.execute("DROP TABLE usuarios")
+
+conn.commit()
--- main.py
+++ main.py
@@ -21,7 +21,7 @@ CREATE TABLE IF NOT EXISTS admin(
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS usuarios(
                id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-               cpf TEXT UNIQUE NOT NULL,
+               email TEXT UNIQUE NOT NULL,
                senha TEXT NOT NULL,
                nome TEXT,
                idade TEXT
@@ -32,18 +32,32 @@ cursor.execute("""
 CREATE TABLE IF NOT EXISTS livros(
                id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
                titulo TEXT NOT NULL,
-               autor TEXT,
-               data_emprestimo TEXT NOT NULL,
-               data_devolução TEXT
+               autor TEXT
                )
 """)
 
-def usuario():
+def cadastrar_user():
+    email = input('Digite seu email: ')
+    senha = input('Digite sua senha: ')
+    nome = input('Digite seu nome: ')
+    idade = input('Digite sua idade: ')
+
+    cursor.execute("INSERT INTO usuarios(email, senha, nome, idade) VALUES(?, ?, ?, ?);",
+                   (email, senha, nome, idade))
+
+
+def emprestar_livros():
+    cursor.execute("""SELECT * FROM livros""")
+    livros = cursor.fetchall()
+
+    print(livros)
+
+def login_usuario():
     nome = input("Nome: ")
-    cpf = input("Digite CPF: ")
+    email = input("Digite email: ")
     senha = input("Senha: ")
-    cursor.execute("SELECT * FROM usuarios WHERE nome = ? AND cpf = ? AND senha = ?;",
-                   (nome, cpf, senha))
+    cursor.execute("SELECT * FROM usuarios WHERE nome = ? AND email = ? AND senha = ?;",
+                   (nome, email, senha))
     validar_usuario = cursor.fetchone()
 
     if validar_usuario:
@@ -72,11 +86,11 @@ def administrador():
         print('3 - deletar item')
         opcoes = input('Select: ')
         if opcoes == '1':
-            print("FUNCIONA")
+            print(emprestar_livros())
         elif opcoes == '2':
-            print("FUNCIONA")
+            print(emprestar_livros())
         elif opcoes == '3':
-            print("FUNCIONA")
+            print(emprestar_livros)
         else:
             print("opção inválida")
     else:
@@ -87,7 +101,14 @@ escolha = input('Select: ')
 if escolha == '1':
     administrador()
 elif escolha == '2':
-    usuario()
+    print('1 - cadastro, 2 - login')
+    selecao = input(': ')
+    if selecao == '1':
+        cadastrar_user()
+        print('Usuário cadastrado com sucesso!')
+        login_usuario()
+    elif selecao == '2':
+        login_usuario()
 
 conn.commit()
 
